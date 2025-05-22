a50c03441f8e8e8ababba06cb95a42ae93edf9ba
MSadi Kufika <manuelsadikufika4@gmail.com>
2025-05-22 22:00:57 +0100

    Add files via upload

---
+++ questions.js
@@ -0,0 +1,612 @@
+let questions = [
+    {
+        numb: 1,
+        question: " Qual é o nome completo do protagonista de Dragon Ball??",
+        answer: "c) Son Goku",
+        options: [
+"a) Vegeta Jr.",
+"b) Kakarotto da Terra",
+"c) Son Goku",
+"d) Ten Shin Han"
+        ]
+    },
+    {
+        numb: 2,
+        question: " Quem foi o primeiro aliado de Goku na infância?",
+        answer: "d) Bulma",
+        options: [
+            "a) Kuririn",
+            "b) Mestre Kame",
+            "c) Yamcha",
+            "d) Bulma"
+        ]
+    },
+    {
+        numb: 3,
+        question: "Qual o nome do pai de Goku?",
+        answer: "b) Bardock",
+        options: [
+            "a) Paragus",
+            "b) Bardock",
+            "c) Raditz",
+            "d) King Vegeta"
+        ]
+    },
+    {
+        numb: 4,
+        question: "Quem foi o responsável por derrotar Cell?",
+        answer: "d) Gohan (em Super Saiyajin 2)",
+        options: [
+            "a) Goku",
+            "b) Trunks do Futuro",
+            "c) Vegeta",
+            "d) Gohan (em Super Saiyajin 2)"
+
+        ]
+    },
+    {
+        numb: 5,
+        question: "Quem Destruir o planeta dos Saiyajin?",
+        answer: "A. Freeze ",
+        options: [
+            "A. Freeze ",
+            "B. Majin Boo",
+            "C. Goku Black",
+            "D. Bills"
+        ]
+    },
+    {
+        numb: 6,
+        question: "Qual o nome do Majin Boo mais puro?",
+        answer: "d) Kid Boo",
+        options: [
+            "a) Super Boo",
+            "b) Evil Boo",
+            "c) Uub",
+            "d) Kid Boo"
+
+        ]
+    },
+    {
+        numb: 7,
+        question: "Qual técnica Gohan usou para derrotar Cell?",
+        answer: "d) Kamehameha Pai e Filho",
+        options: [
+        "a) Genki Dama",
+     "b) Rasengan",
+            "c) Final Flash",
+            "d) Kamehameha Pai e Filho"
+        ]
+    },
+    {
+        numb: 8,
+        question: "Qual é o nome completo do protagonista de One Piece?",
+        answer: "b) Monkey D. Luffy",
+        options: [
+            "a) Portgas D. Ace",
+"b) Monkey D. Luffy",
+"c) Gol D. Roger",
+"d) Shanks"
+
+        ]
+    },
+    {
+        numb: 9,
+        question: "Quem foi o primeiro membro a se juntar ao Luffy?",
+        answer: "d) Roronoa Zoro",
+        options: [
+            "a) Nami",
+"b) Sanji",
+"c) Usopp",
+"d) Roronoa Zoro"
+
+        ]
+    },
+    {
+        numb: 10,
+        question: "Qual é o sonho do Zoro?",
+        answer: "b) Ser o maior espadachim do mundo",
+        options: [
+            
+"a) Ser o rei dos piratas",
+"b) Ser o maior espadachim do mundo",
+"c) Proteger a Nami",
+"d) Encontrar sua família"
+
+        ]
+    },
+    {
+        numb: 11,
+        question: "Qual fruta do diabo Luffy comeu?",
+        answer: "b) Gomu Gomu no Mi",
+        options: [         
+                "a) Mera Mera no Mi",
+                "b) Gomu Gomu no Mi",
+                "c) Suna Suna no Mi",
+                "d) Tori Tori no Mi"
+
+        ]
+    },
+    {
+        numb: 12,
+        question: "Quem derrotou o Kaido?",
+        answer: "b) Luffy (Gear 5, com ajuda)",
+        options: [
+            
+"a) Yamato",
+"b) Luffy (Gear 5, com ajuda)",
+"c) Zoro",
+"d) Law"
+        ]
+    },
+    {
+        numb: 13,
+        question: "Qual é a recompensa atual de Luffy após o arco de Wano?",
+        answer: "b) 3.000.000.000",
+        options: [
+            
+"a) 1.500.000.000",
+"b) 3.000.000.000",
+"c) 4.000.000.000",
+"d) 5.600.000.000"
+
+        ]
+    },
+    {
+        numb: 14,
+        question: "Qual era o nome do navio original do bando?",
+        answer: "d) Going Merry",
+        options: [
+            "a) Thousand Sunny",
+"b) Oro Jackson",
+"c) Red Force",
+"d) Going Merry"
+
+
+        ]
+    },
+    {
+        numb: 15,
+        question: "Qual é o nome completo do protagonista da série?",
+        answer: "a) Naruto Uzumaki",
+        options: [
+            
+"a) Naruto Uzumaki",
+"b) Sasuke Uchiha",
+"c) Kakashi Hatake",
+"d) Minato Namikaze"
+
+        ]
+    },
+    {
+        numb: 16,
+        question: "Qual besta com cauda está selada dentro do Naruto?",
+        answer: "d) Kurama ",
+        options: [
+            "a) Shukaku",
+"b) Gyūki",
+"c) Matatabi",
+"d) Kurama "
+
+        ]
+    },
+    {
+        numb: 17,
+        question: "Qual é a técnica de clonagem usada por Naruto?",
+        answer: "c) Kage Bunshin no Jutsu",
+        options: [
+            "a) Bunshin no Jutsu",
+"b) Kuchiyose no Jutsu",
+"c) Kage Bunshin no Jutsu",
+"d) Shuriken Kage Bunshin"
+
+        ]
+    },
+    {
+        numb: 18,
+        question: "Quem matou Itachi Uchiha?",
+        answer: "b) Sasuke Uchiha (doença terminal)",
+        options: [
+            "a) Kabuto",
+"b) Sasuke Uchiha (doença terminal)",
+"c) Naruto",
+"d) Madara Uchiha"
+
+        ]
+    },
+    {
+        numb: 19,
+        question: "Quem é o criador da Akatsuki original?",
+        answer: "b) Yahiko",
+        options: [
+            "a) Obito",
+"b) Yahiko",
+"c) Pain",
+"d) Konan"
+
+
+        ]
+    },
+    {
+        numb: 20,
+        question: "Qual é o nome verdadeiro do “Pain” que ataca Konoha?",
+        answer: "c) Nagato Uzumaki",
+        options: [
+            "a) Obito Uchiha",
+"b) Yahiko",
+"c) Nagato Uzumaki",
+"d) Zetsu"
+
+        ]
+    },
+    {
+        numb: 21,
+        question: "Qual é o nome completo do protagonista de Bleach?",
+        answer: "c) Ichigo Kurosaki",
+        options: [
+            "a) Renji Abarai",
+            "b) Uryu Ishida",
+            "c) Ichigo Kurosaki",
+            "d) Kisuke Urahara"
+            
+        ]
+    },
+    {
+        numb: 22,
+        question: "Qual é a arma usada pelos Shinigamis?",
+        answer: "c) Zanpakutō",
+        options: [
+            
+"a) Katana comum",
+"b) Shuriken",
+"c) Zanpakutō",
+"d) Reiatsu"
+
+        ]
+    },
+    {
+        numb: 23,
+        question: "Quem deu os poderes de Shinigami ao Ichigo?",
+        answer: "b) Rukia Kuchiki",
+        options: [
+            
+"a) Yoruichi",
+"b) Rukia Kuchiki",
+"c) Soi Fon",
+"d) Orihime Inoue"
+
+        ]
+    },
+    {
+        numb: 24,
+        question: "Qual a forma mais poderosa de um Zanpakutō?",
+        answer: "b) Bankai",
+        options: [
+            "a) Shikai",
+"b) Bankai",
+"c) Reiatsu Pura",
+"d) Getsuga Tensho"
+
+        ]
+    },
+    {
+        numb: 25,
+        question: "Qual é a Hollow interior de Ichigo?",
+        answer: "c) Zangetsu ",
+        options: [
+            "a) Getsuga",
+            "b) Zetsu",
+            "c) Zangetsu ",
+            "d) Espada"
+            
+        ]
+    },
+    {
+        numb: 26,
+        question: "Capitão do 6º esquadrão?",
+        answer: "b) Byakuya Kuchiki",
+        options: [
+            "a) Tōshirō Hitsugaya",
+"b) Byakuya Kuchiki",
+"c) Kenpachi Zaraki",
+"d) Gin Ichimaru"
+
+        ]
+    },
+    {
+        numb: 27,
+        question: "Identidade de Sosuke Aizen após trair a Soul Society?",
+        answer: "a) Ex-capitão do 5º esquadrão e vilão da saga Arrancar",
+        options: [
+            "a) Ex-capitão do 5º esquadrão e vilão da saga Arrancar",
+"b) Capitão da 12ª divisão",
+"c) Shinigami Quincy",
+"d) Vizard infiltrado",
+
+        ]
+    },
+    {
+        numb: 28,
+        question: "Rei dos Quincy e vilão final?",
+        answer: "a) Yhwach",
+        options: [
+            "a) Yhwach",
+"b) Jugram Haschwalth",
+"c) Uryu Ishida",
+"d) Royd Lloyd"
+
+        ]
+    },
+    {
+        numb: 29,
+        question: " Quem derrota Ulquiorra?",
+        answer: "b) Ichigo (forma Vasto Lorde)",
+        options: [
+            
+"a) Uryu Ishida",
+"b) Ichigo (forma Vasto Lorde)",
+"c) Renji",
+"d) Grimmjow"
+
+        ]
+    },
+    {
+        numb: 30,
+        question: "Origem dos poderes de Ichigo?",
+        answer: "c) Fusão de Humano, Shinigami, Quincy e Hollow",
+        options: [
+            "a) Shinigami puro",
+"b) Experimento de Aizen",
+"c) Fusão de Humano, Shinigami, Quincy e Hollow",
+"d) Filho do Rei Quincy"
+
+        ]
+    },
+    {
+        numb: 31,
+        question: "Protagonista principal de Fairy Tail?",
+        answer: "a) Natsu Dragneel",
+        options: [
+            
+"a) Natsu Dragneel",
+"b) Gray Fullbuster",
+"c) Laxus Dreyar",
+"d) Gildarts Clive"
+
+        ]
+    },
+    {
+        numb: 32,
+        question: "Nome da guilda principal?",
+        answer: "a) Fairy Tail",
+        options: [
+            
+"a) Fairy Tail",
+"b) Sabertooth",
+"c) Phantom Lord",
+"d) Blue Pegasus"
+
+        ]
+    },
+    {
+        numb: 33,
+        question: "Gato falante que acompanha Natsu?",
+        answer: "c) Happy",
+        options: [
+            "a) Carla",
+"b) Panther Lily",
+"c) Happy",
+"d) Plue"
+
+        ]
+    },
+    {
+        numb: 34,
+        question: " Rival e amigo de Natsu que também é Dragon Slayer? ",
+        answer: "b) Gajeel Redfox",
+        options: [
+            "a) Sting Eucliffe",
+"b) Gajeel Redfox",
+"c) Rogue Cheney",
+"d) Gray Fullbuster"
+
+        ]
+    },
+    {
+        numb: 35,
+        question: "Quem criou a magia Dragon Slayer?",
+        answer: "b) Dragões como Igneel",
+        options: [
+            
+"a) Acnologia",
+"b) Dragões como Igneel",
+"c) Zeref",
+"d) Makarov"
+
+]
+    },
+    {
+        numb: 36,
+        question: "Nome completo do protagonista?",
+        answer: "a) Gon Freecss",
+        options: [
+            
+"a) Gon Freecss",
+"b) Killua Zoldyck",
+"c) Leorio",
+"d) Kurapika"
+
+        ]
+    },
+    {
+        numb: 37,
+        question: "Melhor amigo de Gon?",
+        answer: "b) Killua Zoldyck",
+        options: [
+            
+"a) Kurapika",
+"b) Killua Zoldyck",
+"c) Leorio",
+"d) Hisoka"
+
+        ]
+    },
+    {
+        numb: 38,
+        question: "Objetivo de Gon ao virar Hunter?",
+        answer: "b) Encontrar seu pai",
+        options: [
+            "a) Vingar sua família",
+"b) Encontrar seu pai",
+"c) Viajar o mundo",
+"d) Derrotar Hisoka",
+
+        ]
+    },
+    {
+        numb: 39,
+        question: "Nome da técnica que manipula energia vital?",
+        answer: "d) Nen",
+        options: [
+            "a) Chakra",
+"b) Reiatsu",
+"c) Haki",
+"d) Nen"
+
+        ]
+    },
+    {
+        numb: 40,
+        question: " Líder da Trupe Fantasma?",
+        answer: "b) Chrollo Lucilfer",
+        options: [
+            "a) Feitan",
+            "b) Chrollo Lucilfer",
+            "c) Hisoka",
+            "d) Phinks"
+            
+        ]
+    },
+    {
+        numb: 41,
+        question: "Protagonista de Jujutsu Kaisen?",
+        answer: "c) Yuji Itadori",
+        options: [
+            "a) Satoru Gojo",
+            "b) Kento Nanami",
+            "c) Yuji Itadori",
+            "d) Megumi Fushiguro"
+            
+        ]
+    },
+    {
+        numb: 42,
+        question: "Melhores amigos de Yuji?",
+        answer: "b) Megumi Fushiguro e Nobara Kugisaki",
+        options: [
+            "a) Maki e Panda",
+"b) Megumi Fushiguro e Nobara Kugisaki",
+"c) Gojo e Geto",
+"d) Todo e Inumaki"
+
+        ]
+    },
+    {
+        numb: 43,
+        question: "O que é um “Domain Expansion”?",
+        answer: "c) Zona de vantagem absoluta da técnica",
+        options: [
+            
+"a) Área selada",
+"b) Técnica de ocultação",
+"c) Zona de vantagem absoluta da técnica",
+"d) Maldição de longa distância"
+
+        ]
+    },
+    {
+        numb: 44,
+        question: "Quem usa “Idle Death Gamble”?",
+        answer: "a) Mahito",
+        options: [
+            
+"a) Mahito",
+"b) Sukuna",
+"c) Kenjaku",
+"d) Jogo"
+
+        ]
+    },
+    {
+        numb: 45,
+        question: "Nome do protagonista de Dr. Stone?",
+        answer: "b) Senku Ishigami",
+        options: [
+            "a) Gen Asagiri",
+            "b) Senku Ishigami",
+            "c) Taiju Oki",
+            "d) Tsukasa Shishio"
+            
+        ]
+    },
+    {
+        numb: 46,
+        question: "O que causou a petrificação?",
+        answer: "b) Luz verde misteriosa",
+        options: [
+            "a) Doença contagiosa",
+            "b) Luz verde misteriosa",
+            "c) Magia de Tsukasa",
+            "d) Erupção solar"
+
+        ]
+    },
+    {
+        numb: 47,
+        question: "Técnica usada para reviver petrificados?",
+        answer: "b) Ácido nítrico / Soro de des-petrificação",
+        options: [
+            "a) Elixir da vida",
+            "b) Ácido nítrico / Soro de des-petrificação",
+            "c) Magia do tempo",
+            "d) Água da ciência"
+
+        ]
+    },
+    {
+        numb: 48,
+        question: "Cientista rival que ajuda Senku depois?",
+        answer: "a) Gen Asagiri",
+        options: [
+
+"a) Gen Asagiri",
+"b) Chrome",
+"c) Ryusui",
+"d) Ukyo"
+
+        ]
+    },
+    {
+        numb: 49,
+        question: "Protagonista principal do Shingeki no kyojin?",
+        answer: "c) Eren Yeager",
+        options: [
+            "a) Armin Arlert",
+"b) Levi Ackerman",
+"c) Eren Yeager",
+"d) Jean Kirstein"
+
+        ]
+    },
+    {
+        numb: 50,
+        question: "Quem Criou este Jogo?",
+        answer: "D. Manuel Sadi Kufika",
+        options: [
+            "A. Pedro Luvangandio Kufika",
+            "B. Nsimba Bernardo Kufika",
+            "C. García Marques Kufika",
+            "D. Manuel Sadi Kufika"
+        ]
+    }
+];
